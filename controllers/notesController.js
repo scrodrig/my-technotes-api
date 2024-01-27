f33328908a51f200225d68119a3ea948f34d0901
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-27 19:37:29 +0000

    Add notesController and noteRouter files

---
+++ controllers/notesController.js
@@ -0,0 +1,124 @@
+const Note = require('../models/Note')
+const User = require('../models/User')
+const asyncHandler = require('express-async-handler')
+
+// @desc    Get all notes
+// @route   GET /notes
+// @access  Private
+
+const getAllNotes = asyncHandler(async (req, res) => {
+    const notes = await Note.find().lean()
+    if (!notes?.length) {
+        return res.status(404).json({ message: 'No notes found' })
+    }
+
+    const notesWithUser = await Promise.all(notes.map(async (note) => {
+        const user = await User.findById(note?.user).lean().exec()
+        //! If user is not found, return 'unknown' as username
+        return { ...note, username: user?.username ?? 'unknown' }
+    }))
+
+    res.json(notesWithUser)
+})
+
+// @desc    Create New Note
+// @route   POST /notes
+// @access  Private
+const createNewNote = asyncHandler(async (req, res) => {
+    const { user, title, text } = req.body
+
+    //Confirm data
+    if (!user || !title) {
+        return res.status(400).json({ message: 'All fields are requiered' })
+    }
+
+    //Check for duplicates
+    const duplicate = await Note.findOne({ title }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Note already exists' })
+    }
+
+    //Create new note
+    const note = await Note.create({ user, title, text })
+
+    if (note) {
+        res.status(201).json({
+            message: `New note ${title} created`
+        })
+    } else {
+        res.status(400).json({ message: 'Invalid note data received' })
+    }
+})
+
+// @desc    Update Note
+// @route   PATCH /notes
+// @access  Private
+const updateNote = asyncHandler(async (req, res) => {
+    const { id, completed, user, title, text } = req.body
+
+    //Confirm data
+    if (!id || !user || !title || !text || typeof completed !== 'boolean') {
+        return res.status(400).json({ message: 'All fields are requiered' })
+    }
+
+    //Check note exists
+    const note = await Note.findById(id).exec()
+
+    if (!note) {
+        return res.status(400).json({ message: 'Note not found' })
+    }
+
+    //* Check for duplicates
+
+    const duplicated = await Note.findOne({ title }).lean().exec()
+
+    //* Allow update if it is the same note
+    if (duplicated && duplicated?._id.toString() !== id) {
+        return res.status(409).json({ message: 'Note already exists' })
+    }
+
+    //Update note
+    note.title = title
+    note.text = text
+    note.completed = completed
+    note.user = user
+
+    const updatedNote = await note.save()
+
+    res.json(`'${updatedNote.title}' updated`)
+
+})
+
+// @desc    Delete Note
+// @route   DELETE /notes
+// @access  Private
+const deleteNote = asyncHandler(async (req, res) => {
+    const { id } = req.body
+
+    //Confirm data
+    if (!id) {
+        return res.status(400).json({ message: 'Note ID required' })
+    }
+
+    //Check note exists
+    const note = await Note.findById(id).exec()
+
+    if (!note) {
+        return res.status(400).json({ message: 'Note not found' })
+    }
+
+    //Delete note
+    const result = await note.deleteOne()
+
+    const reply = `Note '${note.title}' with ID ${note._id} deleted`
+
+    res.json(reply)
+})
+
+module.exports = {
+    getAllNotes,
+    createNewNote,
+    updateNote,
+    deleteNote
+}
--- server.js
+++ server.js
@@ -28,28 +28,29 @@ app.use('/', expresss.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 
 app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRouter'))
 
 app.all('*', (req, res) => {
-  res.status(404)
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
-  } else if (req.accepts('json')) {
-    res.json({ message: '404 Not found' })
-  } else {
-    res.type('txt').send('404 Not found')
-  }
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not found' })
+    } else {
+        res.type('txt').send('404 Not found')
+    }
 })
 
 app.use(errorHandler)
 
 
 mongoose.connection.once('open', () => {
-  console.log("🚀 ~ mongoose.connection.once ~ process.env.NODE_ENV:", process.env.NODE_ENV)
-  console.log('Connected to MongoDB')
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+    console.log("🚀 ~ mongoose.connection.once ~ process.env.NODE_ENV:", process.env.NODE_ENV)
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 })
 
 mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error:', err)
-  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+    console.log('Mongoose connection error:', err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
