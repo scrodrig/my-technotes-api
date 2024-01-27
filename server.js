e7293bc5de73dfb36dc3e9e56bef60967c71009d
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-27 11:56:41 +0000

    Adds index.html and handle routes not defined with

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const expresss = require('express');
 const app = expresss();
+const path = require('path');
 const PORT = process.env.PORT || 3500;
+
+app.use('/', expresss.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root.js'))
+
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not found' })
+  } else {
+    res.type('txt').send('404 Not found')
+  }
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
