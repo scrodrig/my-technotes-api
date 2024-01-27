d08910fcda4977c4d93dd646d6bc9622d6fefc8e
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-27 16:26:53 +0000

    Add AutoIncrement plugin to Note model and update package dependencies

--- models/Note.js
+++ models/Note.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const noteSchema = new mongoose.Schema(
   {
@@ -24,5 +25,11 @@ const noteSchema = new mongoose.Schema(
     timestamps: true
   }
 )
+
+noteSchema.plugin(AutoIncrement, {
+  inc_field: 'ticket',
+  id: 'ticketNums',
+  start_seq: 500
+});
 
 module.exports = mongoose.model('Note', noteSchema);
