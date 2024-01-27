31d688d90796ae1b513956e98c52f6e7f81a9c75
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-27 14:17:47 +0000

    Add cookie-parser dependency and middleware

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const path = require('path');
 const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3500;
 
 
 app.use(logger)
+
+app.use(cors())
 
 app.use(expresss.json())
 
