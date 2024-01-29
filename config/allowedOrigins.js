df8967448e762093dc56a13561dfad8c89aeadc9
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-29 18:56:06 +0000

    fix port for cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
   const allowedOrigins = [
-    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'https://www.myproductionapp.com',
     'https://myproductionapp.com',
   ]
