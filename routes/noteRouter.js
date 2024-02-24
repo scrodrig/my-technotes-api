9f180a4fef3b5c21a274c9e4bad37d9ea761c0b2
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-24 20:53:38 +0000

    Add JWT verification middleware to noteRouter.js

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const noteController = require('../controllers/notesController');
+const verifyJWT = require('../middleware/verifyJWT');
 
-router.route('/')
+router.use(verifyJWT);
+router
+    .route('/')
     .get(noteController.getAllNotes)
     .post(noteController.createNewNote)
     .patch(noteController.updateNote)
-    .delete(noteController.deleteNote)
+    .delete(noteController.deleteNote);
 
 module.exports = router;
