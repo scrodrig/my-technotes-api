8e549f555aeaf234a77a654a0b2d622872088c7a
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 15:31:18 +0000

    update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
   const allowedOrigins = [
-    'https://my-technotes-webapp.onrender.com/',
-    'https://www.my-technotes-webapp.onrender.com/',
+    'https://my-technotes-webapp.onrender.com/'
   ]
 
   module.exports = allowedOrigins
