6ea4c0537d215a974b74063d8ebaefdc11f6421c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-24 15:52:19 +0100

    refactor: Update corsOptions.js to use TypeScript in corsOptions.ts

--- config/corsOptions.ts
+++ config/corsOptions.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-  origin: (origin, callback)=>{
+  origin: (origin: any, callback: (...args: any) => void)=>{
     if(allowedOrigins.indexOf(origin) !== -1 || !origin){
       callback(null, true)
     } else {
